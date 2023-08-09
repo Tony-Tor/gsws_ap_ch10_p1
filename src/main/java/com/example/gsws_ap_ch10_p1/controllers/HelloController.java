00b527553d752df80ed9f94b630715e81dd1b034
TonyTor <Tony.Tor2016@yandex.ru>
2023-08-09 10:10:15 +0400

    ex2 @RestController

--- src/main/java/com/example/gsws_ap_ch10_p1/controllers/HelloController.java
+++ src/main/java/com/example/gsws_ap_ch10_p1/controllers/HelloController.java
@@ -3,18 +3,17 @@ package com.example.gsws_ap_ch10_p1.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    @ResponseBody
     public String hello() {
         return "Hello!";
     }
 
     @GetMapping("/ciao")
-    @ResponseBody
     public String ciao() {
         return "Ciao!";
     }
