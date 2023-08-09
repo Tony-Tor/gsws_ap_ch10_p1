0867dd4336365f95615ace9bb5c7c0abbd2acd02
TonyTor <Tony.Tor2016@yandex.ru>
2023-08-09 09:56:26 +0400

    init

---
+++ src/main/java/com/example/gsws_ap_ch10_p1/GswsApCh10P1Application.java
@@ -0,0 +1,13 @@
+package com.example.gsws_ap_ch10_p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GswsApCh10P1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GswsApCh10P1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gsws_ap_ch10_p1/GswsApCh10P1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsws_ap_ch10_p1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GswsApCh10P1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
