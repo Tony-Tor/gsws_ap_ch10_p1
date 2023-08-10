eb02ab446ad747053c851c0bd68105877982a7f8
TonyTor <Tony.Tor2016@yandex.ru>
2023-08-10 10:26:28 +0400

    ex3 @ResponseEntity<Object>

--- src/main/java/com/example/gsws_ap_ch10_p1/controllers/CountryController.java
+++ src/main/java/com/example/gsws_ap_ch10_p1/controllers/CountryController.java
@@ -1,6 +1,8 @@
 package com.example.gsws_ap_ch10_p1.controllers;
 
 import com.example.gsws_ap_ch10_p1.model.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +12,14 @@ import java.util.List;
 public class CountryController {
 
     @GetMapping("/france")
-    public Country france(){
+    public ResponseEntity<Country> france(){
         Country c = Country.of("France", 67);
-        return c;
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("continent", "Europe")
+                .header("capital", "Paris")
+                .header("favorite_food", "cheese and wine")
+                .body(c);
     }
 
     @GetMapping("/all")
